7973a19515f3c8ba01471d039bbb3c9bfccf87a1
Leo <bruantleo@gmail.com>
2023-01-08 17:33:52 +0100

    Fixed script

--- boot.sh
+++ boot.sh
@@ -3,8 +3,7 @@ sudo pacman -S --noconfirm adapta-gtk-theme alacritty discord docker docker-comp
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm && cd ~ && rm -rf yay &&
 yay -S --noconfirm layan-cursor-theme-git networkmanager-dmenu-git slack-desktop ttf-meslo-nerd-font-powerlevel10k visual-studio-code-bin wired zsh-theme-powerlevel10k-git
 sudo cp ~/projects/Arch-Boot/.gitconfig ~/.gitconfig
-sudo cp ~/projects/Arch-Boot/.p10k.zsh ~/.p10k.zsh
-echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc &&
+sudo cp ~/projects/Arch-Boot/.zshrc ~/.zsh
 git init &&
 git remote add origin https://github.com/LeoBruant/i3-mountains.git &&
 git fetch --all &&
