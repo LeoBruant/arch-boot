7756f6fa34bb7b0d7833c2a365a0b98e5ccd8f2f
Leo <bruantleo@gmail.com>
2023-01-08 19:09:25 +0100

    Added web greeter

--- boot.sh
+++ boot.sh
@@ -1,7 +1,7 @@
 sudo pacman -Syu --noconfirm &&
 sudo pacman -S --noconfirm adapta-gtk-theme alacritty discord docker docker-compose feh firefox git lxappearance neovim papirus-icon-theme picom polybar ripgrep rofi thunar zsh &&
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm && cd ~ && rm -rf yay &&
-yay -S --noconfirm layan-cursor-theme-git networkmanager-dmenu-git slack-desktop ttf-meslo-nerd-font-powerlevel10k visual-studio-code-bin wired zsh-theme-powerlevel10k-git
+yay -S --noconfirm layan-cursor-theme-git networkmanager-dmenu-git slack-desktop ttf-meslo-nerd-font-powerlevel10k visual-studio-code-bin web-greeter wired zsh-theme-powerlevel10k-git
 sudo cp ~/projects/Arch-Boot/.gitconfig ~/.gitconfig
 sudo cp ~/projects/Arch-Boot/.zshrc ~/.zsh
 git init &&
