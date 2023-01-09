5558a8fd14c6c8f800f5e47e4c944de157aba131
Leo <bruantleo@gmail.com>
2023-01-09 13:35:27 +0100

    Added comments

--- boot.sh
+++ boot.sh
@@ -1,11 +1,28 @@
-sudo pacman -Syu --noconfirm &&
-sudo pacman -S --noconfirm adapta-gtk-theme alacritty discord docker docker-compose feh firefox git lxappearance neovim papirus-icon-theme picom polybar ripgrep rofi thunar zsh &&
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm && cd ~ && rm -rf yay &&
+# Update
+sudo pacman -Syu --noconfirm
+
+# Install packages
+sudo pacman -S --noconfirm adapta-gtk-theme alacritty discord docker-compose feh firefox git lxappearance neovim papirus-icon-theme picom polybar ripgrep rofi thunar zsh
+
+# Install yay
+cd ~ && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm && cd ~ && rm -rf yay
+
+# Install AUR packages
 yay -S --noconfirm layan-cursor-theme-git networkmanager-dmenu-git slack-desktop ttf-meslo-nerd-font-powerlevel10k visual-studio-code-bin web-greeter wired zsh-theme-powerlevel10k-git
+
+# Enable services
+sudo systemctl enable docker
+#gpasswd -a user docker
+
+# Get theme
+git init
+git remote add origin https://github.com/LeoBruant/i3-mountains.git
+git fetch --all
+git reset --hard origin/master
+
+# Copy config files
 sudo cp ~/projects/Arch-Boot/.gitconfig ~/.gitconfig
 sudo cp ~/projects/Arch-Boot/.zshrc ~/.zsh
-git init &&
-git remote add origin https://github.com/LeoBruant/i3-mountains.git &&
-git fetch --all &&
-git reset --hard origin/master &&
+
+# Restart shell
 exec zsh
