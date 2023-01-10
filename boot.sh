08cf9ade555f64c5845b2bd38d39415d6ec992d8
Leo <bruantleo@gmail.com>
2023-01-10 19:20:33 +0100

    Update

--- boot.sh
+++ boot.sh
@@ -10,22 +10,19 @@ cd ~ && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --n
 # Install AUR packages
 yay -S --noconfirm layan-cursor-theme-git networkmanager-dmenu-git papirus-folders-nordic slack-desktop ttf-meslo-nerd-font-powerlevel10k visual-studio-code-bin web-greeter wired zsh-theme-powerlevel10k-git
 
-# Enable services
+# Enable docker
 sudo systemctl enable docker
-#gpasswd -a user docker
+sudo groupadd docker && sudo usermod -aG docker $USER
+
+# Copy config files
+sudo cp ~/projects/Arch-Boot/.gitconfig ~/.gitconfig
 
 # Get theme
 git init
 git remote add origin https://github.com/LeoBruant/i3-mountains.git
-git fetch --all
-git reset --hard origin/master
-sudo mkdir /usr/share/backgrounds
-sudo mkdir /usr/share/backgrounds/archlinux-login-backgrounds
-chmod +x config.sh
-sudo ./config.sh
-
-# Copy config files
-sudo cp ~/projects/Arch-Boot/.gitconfig ~/.gitconfig
+git fetch --all && git reset --hard origin/master
+sudo mkdir /usr/share/backgrounds && sudo mkdir /usr/share/backgrounds/archlinux-login-backgrounds
+chmod +x scripts/config.sh && sudo ./scripts/config.sh
 
 # Restart shell
 exec zsh
