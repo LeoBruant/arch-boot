a8f4056d0270da29920714bedda4629cad7115ee
Leo <bruantleo@gmail.com>
2023-01-17 21:11:18 +0100

    Added autotiling

--- boot.sh
+++ boot.sh
@@ -8,7 +8,7 @@ sudo pacman -S --noconfirm adapta-gtk-theme alacritty discord docker docker-comp
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm && cd .. && rm -rf yay
 
 # Install AUR packages
-yay -S --noconfirm layan-cursor-theme-git networkmanager-dmenu-git nvchad-git papirus-folders-nordic slack-desktop ttf-meslo-nerd-font-powerlevel10k visual-studio-code-bin web-greeter wired zsh-theme-powerlevel10k-git
+yay -S --noconfirm autotiling layan-cursor-theme-git networkmanager-dmenu-git nvchad-git papirus-folders-nordic slack-desktop ttf-meslo-nerd-font-powerlevel10k visual-studio-code-bin web-greeter wired zsh-theme-powerlevel10k-git
 
 # Enable docker
 sudo systemctl enable docker
