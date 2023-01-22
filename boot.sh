1198c8d16a9776b22fb79743ea216469ca4d7b99
Leo <bruantleo@gmail.com>
2023-01-22 13:09:23 +0100

    Update

--- boot.sh
+++ boot.sh
@@ -24,7 +24,7 @@ git init
 git remote add origin https://github.com/LeoBruant/i3-mountains.git
 git fetch --all && git reset --hard origin/master
 sudo mkdir /usr/share/backgrounds && sudo mkdir /usr/share/backgrounds/archlinux-login-backgrounds
-chmod +x scripts/config.sh && sudo ./scripts/config.sh
+chmod +x scripts/config.sh && ./scripts/config.sh
 alacritty &
 exit
 
