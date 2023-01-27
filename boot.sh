f893c17c609c15755db5f90fe29ae31ebd1edee3
Leo <bruantleo@gmail.com>
2023-01-27 22:19:08 +0100

    Updated gtk theme

--- boot.sh
+++ boot.sh
@@ -2,13 +2,18 @@
 sudo pacman -Syyu --noconfirm
 
 # Install packages
-sudo pacman -S --noconfirm adapta-gtk-theme alacritty discord docker docker-compose dunst feh firefox flameshot lsd neofetch neovim picom polybar ripgrep rofi thunar zsh
+sudo pacman -S --noconfirm alacritty discord docker docker-compose dunst feh firefox flameshot lsd neofetch neovim picom polybar ripgrep rofi thunar zsh zsh-syntax-highlighting zsh-theme-powerlevel10k
 
 # Install yay
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm && cd .. && rm -rf yay
 
 # Install AUR packages
-yay -S --noconfirm auto-cpufreq autotiling layan-cursor-theme-git networkmanager-dmenu-git nvchad-git papirus-icon-theme-stripped  slack-desktop ttf-meslo-nerd-font-powerlevel10k visual-studio-code-bin web-greeter zsh-theme-powerlevel10k-git
+yay -S --noconfirm auto-cpufreq autotiling catppuccin-gtk-theme-mocha layan-cursor-theme-git networkmanager-dmenu-git nvchad-git papirus-icon-theme-stripped  slack-desktop ttf-meslo-nerd-font-powerlevel10k visual-studio-code-bin web-greeter
+
+# Enable docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+newgrp dock
 
 # Copy config files
 mv ~/projects/Arch-Boot/.gitconfig ~/.gitconfig
@@ -24,11 +29,6 @@ sudo mkdir /usr/share/backgrounds/archlinux-login-backgrounds
 sudo mv ~/.config/lightdm.conf /etc/lightdm/lightdm.conf
 sudo mv ~/.config/lightdm.jpg /usr/share/backgrounds/archlinux-login-backgrounds/lightdm.jpg
 alacritty &
-
-# Enable docker
-sudo systemctl enable docker
-sudo usermod -aG docker $USER
-newgrp dockere
 
 exit
 
