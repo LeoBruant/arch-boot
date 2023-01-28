238cf11cb910d7696d18b686aa53e81be623fbb4
LeoBruant <bruantleo@gmail.com>
2023-01-28 21:03:22 +0100

    Update

--- boot.sh
+++ boot.sh
@@ -2,21 +2,31 @@
 sudo pacman -Syyu --noconfirm
 
 # Install packages
-sudo pacman -S --noconfirm alacritty discord docker docker-compose dunst feh firefox flameshot lsd neofetch neovim picom polybar ripgrep rofi thunar zsh zsh-syntax-highlighting zsh-theme-powerlevel10k
+sudo pacman -S --noconfirm adapta-gtk-theme alacritty discord docker docker-compose dunst feh firefox flameshot lsd neofetch neovim picom polybar ripgrep rofi thunar zsh
 
 # Install yay
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm && cd .. && rm -rf yay
 
 # Install AUR packages
-yay -S --noconfirm auto-cpufreq autotiling catppuccin-gtk-theme-mocha layan-cursor-theme-git networkmanager-dmenu-git nvchad-git papirus-icon-theme-stripped  slack-desktop ttf-meslo-nerd-font-powerlevel10k visual-studio-code-bin web-greeter
+yay -S --noconfirm auto-cpufreq autotiling layan-cursor-theme-git networkmanager-dmenu-git nvchad-git papirus-icon-theme-stripped  slack-desktop ttf-meslo-nerd-font-powerlevel10k visual-studio-code-bin web-greeter zsh-theme-powerlevel10k-git
 
-# Enable docker
-sudo systemctl enable docker
-sudo usermod -aG docker $USER
-newgrp dock
-
-# Copy config files
+# Git config
 mv ~/projects/Arch-Boot/.gitconfig ~/.gitconfig
+git config --global user.email "bruantleo@gmail.com"
+git config --global user.name "LeoBruant"
+
+# SSH key
+ssh-keygen -t ed25519 -C "bruantleo@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+cat ~/.ssh/id_ed25519.pub
+
+# GPG key
+gpg --full-generate-key
+gpg=$(gpg --list-secret-keys --keyid-format=long | grep "sec" | grep -o "/[A-Za-z0-9]*" | grep -o "[A-Za-z0-9]*")
+gpg --armor --export $gpg
+git config --global user.signingkey $gpg
+git config --global commit.gpgsign true
 
 # Get theme
 cd
@@ -30,5 +40,9 @@ sudo mv ~/.config/lightdm.conf /etc/lightdm/lightdm.conf
 sudo mv ~/.config/lightdm.jpg /usr/share/backgrounds/archlinux-login-backgrounds/lightdm.jpg
 alacritty &
 
-exit
+# Enable docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+newgrp dockere
 
+exit
